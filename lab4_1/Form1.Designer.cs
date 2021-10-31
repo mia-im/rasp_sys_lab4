717d30c9461296f98945944693e478e20b789d57
mia-im <62039441+mia-im@users.noreply.github.com>
2021-10-31 03:23:16 +0300

    Add files via upload

---
+++ lab4_1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace lab4_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(25, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(258, 306);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(357, 13);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(258, 306);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(690, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 306);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Отрисовка прямоугольников";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(391, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Отрисовка элипсов";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(728, 325);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 37);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Сгенерировать числа";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(318, 398);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 54);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Запустить все методы";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(526, 398);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(138, 54);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Остановить выполнение";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 464);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ lab4_1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace lab4_1
+{
+    public partial class Form1 : Form
+    {
+        Thread thread1;
+        Thread thread2;
+        Thread thread3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            thread1 = new Thread(new ThreadStart(DrawRectangle));
+            thread2 = new Thread(new ThreadStart(DrawEllipse));
+            thread3 = new Thread(new ThreadStart(RandomNumber));
+        }
+        //Прорисовка прямоугольников
+        private void DrawRectangle()
+        {
+            try
+            {
+                Random rnd = new Random();
+                Graphics g = panel1.CreateGraphics();
+                while (true)
+                {
+                    Thread.Sleep(50);
+                    g.DrawRectangle(Pens.Pink, 0, 0, rnd.Next(this.Width),
+                    rnd.Next(this.Height));
+                }
+            }
+            catch (Exception ex) { }
+        }
+        //Прорисовка элипсов
+        private void DrawEllipse()
+        {
+            try
+            {
+                Random rnd = new Random();
+                Graphics g = panel2.CreateGraphics();
+                while (true)
+                {
+                    Thread.Sleep(50);
+                    g.DrawEllipse(Pens.Yellow, 0, 0, rnd.Next(this.Width),
+                    rnd.Next(this.Height));
+                }
+            }
+            catch (Exception ex) { }
+        }
+        //Запись чисел
+        private void RandomNumber()
+        {
+            try
+            {
+                Random rnd = new Random();
+                Parallel.For(0, 500, i =>
+                {
+                    textBox1.Invoke((MethodInvoker)delegate ()
+                    {
+                        textBox1.Text += rnd.Next().ToString();
+                    });
+                });
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        //Кнопка запуска всех трех методов
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                thread1.Start();
+                thread2.Start();
+                thread3.Start();
+            }
+            catch (Exception ex) { }
+        }
+        //Запуск метода прорисовки прямоугольников
+        private void button1_Click(object sender, EventArgs e)
+        {
+            thread1.Start();
+        }
+        //Запуск метода прорисовки элипсов
+        private void button2_Click(object sender, EventArgs e)
+        {
+            thread2.Start();
+        }
+        //Запуск метода записи чисел
+        private void button3_Click(object sender, EventArgs e)
+        {
+            thread3.Start();
+        }
+        //Приостановка потоков
+        private void button5_Click(object sender, EventArgs e)
+        {
+            thread1.Interrupt();
+            thread2.Interrupt();
+            thread3.Interrupt();
+        }
+        //Код при закрытии формы
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+    }
+}
